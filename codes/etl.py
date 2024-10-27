6cda12dbcbe2d004bbce2cb2380a36050cecc411
heshara dananjanee <hesharadananjanee@gmail.com>
2024-10-28 00:21:44 +0530

    ETL process learning

---
+++ codes/etl.py
@@ -0,0 +1,42 @@
+"""sumary_line
+python extract transform load example
+"""
+import requests
+import sqlalchemy
+
+import pandas as pd #transformation,manupulation
+from sqlalchemy import create_engine #create a connection to a datbase
+
+def extract()-> dict:
+    """ This API extracts data from
+    http://universities.hipolabs.com
+    """
+    API_URL = "http://universities.hipolabs.com/search?country=United+States"
+    data = requests.get(API_URL).json()
+    return data
+def transform(data:dict) -> pd.DataFrame:
+    """transforms the dataset into desired data structure and filter
+    """
+    df = pd.DataFrame(data)
+    print(f"Total Number of universities from API {len(data)}")
+    df = df[df["name"].str.contains("California")]
+    print(f"Number of universities in california {len(df)}")
+    df['domains'] = [','.join(map(str, l)) for l in df['domains']]
+    df['web_pages'] = [','.join(map(str, l)) for l in df['web_pages']]
+    df = df.reset_index(drop=True)
+    return df[["domains","country","web_pages","name"]]
+
+def load(df:pd.DataFrame)-> None:
+    """ Loads data into a sqllite database"""
+    disk_engine = create_engine('sqlite:///my_lite_store.db')
+    df.to_sql('cal_uni', disk_engine, if_exists='replace')
+
+# %%
+data = extract()
+df = transform(data)
+load(df)
+
+    
+
+
+# %%
