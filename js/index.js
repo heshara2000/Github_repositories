a3bf28bad321c8552a1031f36bbc6b93fd5b0646
heshara dananjanee <hesharadananjanee@gmail.com>
2024-10-31 00:31:02 +0530

    js files

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// var x = 7;
+
+// function getName(){
+//     console.log("hello javascript");
+// }
+
+// getName();
+// console.log(x);
+
+// var x = 7;
+
+// function getName(){
+//     console.log("hello javascript");
+// }
+
+// getName();
+// console.log(x);
+
+var x=1;
+a();
+b();
+
+console.log(x);
+
+function a(){
+    var x=10;
+    console.log(x);
+}
+
+function b(){
+    var x=100;
+    console.log(x);
+}
