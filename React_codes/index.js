e1967713d45212fbd687a92ad16f3740131c9eae
heshara dananjanee <hesharadananjanee@gmail.com>
2024-11-02 00:14:28 +0530

    add react

---
+++ React_codes/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const navbar (
+    <nav>
+        <h1>Hesh</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar,document.getElementById("root"))
