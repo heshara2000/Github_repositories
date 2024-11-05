ab3313f1d81a4bbd550b6ecb62820d3057372422
heshara dananjanee <hesharadananjanee@gmail.com>
2024-11-06 01:42:38 +0530

    node js

---
+++ node js/app.js
@@ -0,0 +1,62 @@
+// const amount = 9
+
+// if (amount < 10) {
+//     console.log('small number')
+// }
+// else {
+//     console.log('large number')
+// }
+
+// console.log('its my first node app!!!')
+
+// const name = require('./names')
+// const data = require('./alternative')
+// const sayHi = require('./utils')
+// const data1 = require('./mind-grenade')
+
+// sayHi(john)
+// sayHi(peter)
+
+// const os = require('os')
+// const user = os.userInfo()
+// console.log(user)
+
+// //method returns the system uptime in seconds
+// console.log('the system uptime is ${os.uptime()} seconds')
+
+// const currentOs = {
+//     name: os.type(),
+//     release: os.release(),
+//     totalMem: os.totalmem(),
+
+
+//     }
+
+//##############
+// const path =require('path')
+// console.log(path.sep)
+
+// const filepath = path.join('/content','subfolder','text.txt')
+
+// console.log(filepath)
+
+// const {readFileSync, writeFileSync} = require('fs')
+// const first = readFileSync('./content/first.txt', 'utf8')
+// const second = readFileSync('./content/second.txt','utf8')
+
+//###################
+
+// npm --version
+// local dependency - use it only in this particular project
+// npm i <packagename>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+
+// package.json - mainfest file (stores important info about project/package)
+// manual approach (create package.json in the ShadowRoot, create properties etc)
+// npm init (step by step , press enter to skip)
+// npm init -y (everything default)
+
+
+
---
+++ node js/intro.js
@@ -0,0 +1,14 @@
+
+//__dirname - patht o current doirectory
+//__filename - file name
+//require - function to use the module
+//module - info about current module
+
+
+
+
+
+console.log(__dirname)
+setInterval(()=> {
+    console.loh('hello world')
+}, 1000)
